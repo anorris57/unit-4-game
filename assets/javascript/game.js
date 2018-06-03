cd642da86244352423c28625c1096ae254b0db2b
Angela Norris <cocoanorris@yahoo.com>
2018-06-03 15:34:08 -0400

    added data-type so that calculations can be done

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,9 +7,7 @@ var blueRandomNumber;
 var diamondRandomNumber;
 var lovelyRandomNumber;
 var lovelyRandomNumber;
-//var totalScore = 0;
-//var count = 0;
-//var blueCount;
+var counter = 0;
 
   controlRandomNumber = Math.floor((Math.random() * 121) + 18);
   $("#control").text(controlRandomNumber);
@@ -30,12 +28,53 @@ var lovelyRandomNumber;
   console.log(lovelyRandomNumber);
   purpleRandomNumber = getRndInteger(1,12);
   console.log(purpleRandomNumber);
+
+//1
+$("#img1").attr("data-value", blueRandomNumber);
+
+$(".one").text($("#img1").attr("data-value"));
+
+//2
+$("#img2").attr("data-value", diamondRandomNumber);
+
+$(".two").text($("#img2").attr("data-value"));
+
+//3
+$("#img3").attr("data-value", lovelyRandomNumber);
+ 
+$(".three").text($("#img3").attr("data-value"));
+
+//4
+$("#img4").attr("data-value", purpleRandomNumber);
+
+$(".four").text($("#img4").attr("data-value"));
+
+
+//html.12
+$(".crystal-image").on("click", function() {
+
+  var crystalValue = ($(this).attr("data-value"));
+  crystalValue = parseInt(crystalValue);
+  // We then add the crystalValue to the user's "counter" which is a global variable.
+  // Every click, from every crystal adds to the global counter.
+
+  counter += crystalValue;
+
+  // All of the same game win-lose logic applies. So the rest remains unchanged.
+  alert("New score: " + counter);
+
+  if (counter === controlRandomNumber) {
+    alert("You win!");
+  }
+
+  else if (counter >= controlRandomNumber) {
+    alert("You lose!!");
+  }
+
+});
   
  
-  $("#blue").on("click", function(){
-    count++;
-  })
-  
+ 
 
 
   
@@ -43,13 +82,7 @@ var lovelyRandomNumber;
 
   
  
-  /*when clicking image add number to totalScore
-  $("#blue").on("click", function(){
-       count++;
-       var blueCount = count;
-       alert(blueCount);
-       totalScore = totalScore + blueRandomNumber;
-  })*/
+ 
   
 
 
